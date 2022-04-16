2b34d0a08805099ed177a2202e25ab3435e35b2c
Higor Miguel <higor.mig@gmail.com>
2022-04-16 17:23:04 -0300

    feat: created pivot table (many-to-many)

---
+++ prisma/migrations/20220416201914_pivot_table_many_to_manmy/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "courses_modules" (
+    "id" TEXT NOT NULL,
+    "fk_id_course" TEXT NOT NULL,
+    "fk_id_module" TEXT NOT NULL,
+
+    CONSTRAINT "courses_modules_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "courses_modules" ADD CONSTRAINT "courses_modules_fk_id_course_fkey" FOREIGN KEY ("fk_id_course") REFERENCES "courses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "courses_modules" ADD CONSTRAINT "courses_modules_fk_id_module_fkey" FOREIGN KEY ("fk_id_module") REFERENCES "modules"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
